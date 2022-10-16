20d5ee91e83cb8bbc12a6aabf6b92f728449fc09
MartinOpa <113948972+MartinOpa@users.noreply.github.com>
2022-10-16 17:14:55 +0200

    Add files via upload

---
+++ ukol2/common.mk
@@ -0,0 +1,20 @@
+OUT = bin
+
+SEARCHPATH += src
+vpath %.cpp $(SEARCHPATH)
+vpath %.h $(SEARCHPATH)
+
+DEPS += main.h
+
+_OBJS += main.o
+
+OBJS = $(patsubst %,$(OUT)/%,$(_OBJS))
+
+all: $(PROG)
+
+$(OUT)/%.o: %.cpp %.h $(DEPS)
+	@mkdir -p $(OUT)
+	$(CC) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
+	
+clean:
+	$(RM) -rf $(OUT) $(PROG)
---
+++ ukol2/src/main.h
@@ -0,0 +1,5 @@
+#include <vector>
+#include <iostream>
+#include "polynomial.cpp"
+
+using namespace std;
